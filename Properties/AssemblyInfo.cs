ebda50c0dcb7f9db1ca8358ef9893dacaaa742b4
Simon Mourier <simon_mourier@hotmail.com>
2026-01-03 16:34:37 +0100

    wip

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CSharpMerge")]
 [assembly: AssemblyDescription("A console utility to merge .cs source files from a directory into one unique .cs file.")]
 [assembly: AssemblyProduct("CSharpMerge")]
-[assembly: AssemblyCopyright("Copyright © 2017-2025 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2017-2026 Simon Mourier. All rights reserved.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("253bd4d7-2b86-43c0-827d-0fce5f746ba0")]
 [assembly: AssemblyVersion("1.8.0.0")]
